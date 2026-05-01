d1962c603310c899feccc3fd438f8d4a2c66f564
littints <littints@192.168.1.4>
2026-05-01 18:51:54 +0530

    Java Operator push

---
+++ Operator/src/Operator.java
@@ -0,0 +1,43 @@
+
+public class Operator {
+	public static void main(String[] arg){
+		int a=7;
+		int b=2;
+		float c;
+		c=(float)a/b;
+		int e=++a;
+		int d=b++;
+		int age=20;
+		
+	    System.out.println("Aritamatic operator");
+		System.out.println("SUM : "+ a +"+" +b +"" +"="+(a+b));
+		System.out.println("SUBSTRACTION : "+a +"-" +b +"="+(a-b));
+		System.out.println("MULTIPLICATION : "+a +"*" +b +"="+(a*b));
+		System.out.println("DIVISION : "+a +"/" +b +"="+c);
+		System.out.println("MODULAS : "+a +"%" +b +"="+(a%b));
+		System.out.println(e);
+		System.out.println(d);
+		System.out.println(b);
+		System.out.println("Relational Operator");
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println("Assignment Operator");
+		System.out.println(a+=b);
+		System.out.println(a-=b);
+		System.out.println(a*=b);
+		System.out.println(a%=b);
+		System.out.println(a/=b);
+		if(age>18);
+		{
+			System.out.println("Eligibile for vote");
+		}
+		System.out.println("HI WELCOME TO VOTING WORLD");
+
+		
+	}
+
+}
